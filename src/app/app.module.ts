86ce55f86f7bb32117c96a2170ad233707632d91
Lina Eweis <linaeweis@gmail.com>
2020-10-09 18:14:44 +0200

    initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { HeaderComponent } from './main-layout/header/header.component';
+import { FooterComponent } from './main-layout/footer/footer.component';
+import { MenuComponent } from './main-layout/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
