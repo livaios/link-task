0ff88f6705a15c7b7b41ee4d12b1fedf330e0d9a
Lina Eweis <linaeweis@gmail.com>
2020-10-12 14:03:25 +0200

    hotfixes

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +7,13 @@ import { Injectable } from '@angular/core'
 export class GlobalService {
   path = 'Home'
   selectedArticle: any
-  constructor() {}
+  constructor(private route: Router) {}
+  navToPress() {
+    console.log('click')
+    this.route.navigateByUrl('/press_release')
+  }
+  readMore(article) {
+    this.navToPress()
+    this.selectedArticle = article
+  }
 }
